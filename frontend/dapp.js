782ad5c4b18fbd20833ceb502d14578f46dfa571
Mike Ghen <mike@mikeghen.com>
2021-02-27 12:52:20 -0500

    Added my contract

--- frontend/dapp.js
+++ frontend/dapp.js
@@ -1,5 +1,5 @@
 // Change this address to match your deployed contract!
-const contract_address = "0xa5b70b3f9e02b2d0b85E10042a76b1A7F397Cc6b";
+const contract_address = "0x17eF2E0cD056F2BEbcEbebEb615FB4c2234eCB9C";
 
 const dApp = {
   ethEnabled: function() {
